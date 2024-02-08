f9498733fc95215e51e0910683fe002b4dcdf23e
Md. Sohan Millat Sakib <millatsakib01@gmail.com>
2024-02-08 12:46:25 +0600

    4th problem solved

--- index.js
+++ index.js
@@ -11,7 +11,6 @@ function calculateMoney(ticketSale) {
 }
 
 
-
 function checkName(name) {
     if (typeof name !== "string") {
         return "invalid";
@@ -37,12 +36,31 @@ function deleteInvalids(array) {
     return antiVirusArray;
 }
 
+function password(obj) {
+    if (typeof obj.name === "undefined" || typeof obj.birthYear === "undefined" || typeof obj.siteName === "undefined") {
+        return "invalid";
+    }
+    let birthYearDigit = obj.birthYear.toString();
+    if (birthYearDigit.length !== 4) {
+        return "invalid";
+    }
+    let generatedPass;
+    let capitalizeSiteName;
+    capitalizeSiteName = obj.siteName.charAt(0).toUpperCase();
+    for (let i = 1; i < obj.siteName.length; i++) {
+        capitalizeSiteName += obj.siteName[i].toLowerCase();
+    }
+    generatedPass = capitalizeSiteName + "#" + obj.name + "@" + birthYearDigit;
+    return generatedPass;
+}
+
 
 /* It must be deleted bofore submit*/
-var calculateT = calculateMoney(10)
-console.log(calculateT);
-console.log(checkName(['Rashed']));
-console.log(deleteInvalids({ num: [1, 2, 3] }));
+// var calculateT = calculateMoney(10)
+// console.log(calculateT);
+// console.log(checkName(['Rashed']));
+// console.log(deleteInvalids({ num: [1, 2, 3] }));
+// console.log(password({ name: "sakib", birthYear: 1545, siteName: "google" }));
 // let abc = NaN;
 // // console.log(String(abc) === 'NaN');
 // if (abc.toString() === 'NaN') {
